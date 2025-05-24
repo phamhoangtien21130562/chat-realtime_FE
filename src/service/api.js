43d726a1545e5861d498ebf34482275fad62ad52
22130136@st.hcmuaf.edu.vn <144929114+nguyen-tuankiet@users.noreply.github.com>
2025-05-24 20:42:12 +0700

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://sorfware-message.onrender.com';
-const API_URL = 'http://localhost:8080/';
+const API_URL = 'https://sorfware-message.onrender.com';
+// const API_URL = 'http://localhost:8080/';
 
 const api = axios.create({
     baseURL: API_URL,
